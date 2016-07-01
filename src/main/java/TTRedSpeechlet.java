15977325463298ce6ac3a5b219a203e991b0118b
Az <solendev@gmail.com>
2016-06-30 18:46:39 -0700

    refactored onIntent()

--- src/main/java/TTRedSpeechlet.java
+++ src/main/java/TTRedSpeechlet.java
@@ -36,28 +36,24 @@ public class TTRedSpeechlet implements Speechlet {
     @Override
     public void onSessionStarted(final SessionStartedRequest request, final Session session)
             throws SpeechletException {
-        log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(),
-                session.getSessionId());
+        log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
         // any initialization logic goes here
     }
 
     @Override
     public SpeechletResponse onLaunch(final LaunchRequest request, final Session session)
             throws SpeechletException {
-        log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(),
-                session.getSessionId());
+        log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
         return getWelcomeResponse(session);
     }
     
  
-    @Override //TODO: refactor this onIntent() method to be much smaller.
+    @Override
     public SpeechletResponse onIntent(final IntentRequest request, final Session session)
             throws SpeechletException {
-    	log.info("onIntent requestId={}, sessionId={}", request.getRequestId(),
-		        session.getSessionId());
+    	log.info("onIntent requestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
 		
 		Intent incomingIntent = request.getIntent();
-		TTRedStateManager manager = new TTRedStateManager();
 
 		if (TTIntent.INTENT_HELP.name.equals(incomingIntent.getName())) { //"AMAZON.HelpIntent" sample utterances: help,help me,can you help me. Provide help about how to use the skill. See “Offer Help for Complex Skills” in the Voice Design Best Practices for guidelines about help text.
 			return getHelpAskResponse();
@@ -66,41 +62,21 @@ public class TTRedSpeechlet implements Speechlet {
 		} 
         
 		// Get the current state the session, get current state node
-		String currentState = (String) session.getAttribute(AZConst.SESSION_KEY_CURRENT_STATE.getValue());
-		AZStateNode<String> currentStateNode = null;
-		currentStateNode = manager.getRootNode();
+		AZStateNode<String> currentStateNode = getCurrentStateNode(session);
 		
-		if (!StringUtils.isNotEmpty(currentState)) {
-		    currentState = AZConst.INITIAL_STATE.getValue();//initial state is when the skill/app is launched, the root node in story tree
-		    session.setAttribute(AZConst.SESSION_KEY_CURRENT_STATE.getValue(),currentState);
-		} else {
-		  currentStateNode = manager.getNodeMatchingDebugName(currentStateNode, currentState);
-		}
-		
-		String previousIntent = (String) session.getAttribute(AZConst.SESSION_KEY_PREVIOUS_INTENT.getValue());
-		String debugString = "currentStateAtStartOfTick="+currentState+",previousIntent="+previousIntent+",incomingIntent="+incomingIntent.getName();
 		//See if valid transition matches current state
 		String speechOutput = null;
 		if(currentStateNode.transferStructure.hasValidTransferForIntent(incomingIntent)){
-		    //Set new state if match, else- error prompt
 		    AZStateNode<String> nextState = currentStateNode.transferStructure.getStateForIntent(incomingIntent);
-		    session.setAttribute(AZConst.SESSION_KEY_CURRENT_STATE.getValue(), nextState.debugName);//fire transition!
-		    debugString = "newCurrentStateAfterTransition="+nextState.debugName+","+debugString;
+		    session.setAttribute(AZConst.SESSION_KEY_CURRENT_STATE.getValue(), nextState.debugName);//setting the "CURRENT_STATE" in the session ensures the transition is executed
 		    currentStateNode = nextState;
 		    speechOutput = currentStateNode.audioContainer.getSSML();
 		} else {
 		    speechOutput = currentStateNode.audioContainer.getErrorSSML();
 		}
-		session.setAttribute("DEBUG_AZ",debugString);//tmp!
 		
-		//update the previous intent
-		session.setAttribute(AZConst.SESSION_KEY_PREVIOUS_INTENT.getValue(),incomingIntent.getName());
-		
-		String statePathKey = (String) session.getAttribute(AZConst.SESSION_KEY_STATE_PATH.getValue());
-		statePathKey = (statePathKey == null) ? "" : statePathKey;
-		session.setAttribute(AZConst.SESSION_KEY_STATE_PATH.getValue(),statePathKey+","+currentStateNode.debugName);//path taken in entire session!
-		//TODO: save (currentState, incomingIntent) KV pairs for future debugging in time series within the session
-		
+		saveDebugInfoToSession(session, currentStateNode, incomingIntent);
+						
 		Image introImage = new Image();
 		introImage.setLargeImageUrl(currentStateNode.cardData.largeImageUrl);
 		introImage.setSmallImageUrl(currentStateNode.cardData.smallImageUrl);
@@ -135,8 +111,7 @@ public class TTRedSpeechlet implements Speechlet {
     @Override
     public void onSessionEnded(final SessionEndedRequest request, final Session session)
             throws SpeechletException {
-        log.info("onSessionEnded requestId={}, sessionId={}", request.getRequestId(),
-                session.getSessionId());
+        log.info("onSessionEnded requestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
         // any cleanup logic goes here
     }
 
@@ -217,4 +192,37 @@ public class TTRedSpeechlet implements Speechlet {
         
     }
 
+    private AZStateNode<String> getCurrentStateNode(final Session session){
+        TTRedStateManager manager = new TTRedStateManager();
+		String currentState = (String) session.getAttribute(AZConst.SESSION_KEY_CURRENT_STATE.getValue());
+		if (!StringUtils.isNotEmpty(currentState)) {
+		    currentState = AZConst.INITIAL_STATE.getValue();//initial state is when the skill/app is launched, the root node in story tree
+		    session.setAttribute(AZConst.SESSION_KEY_CURRENT_STATE.getValue(),currentState);
+			return manager.getRootNode();
+		} else {
+		  return manager.getNodeMatchingDebugName(manager.getRootNode(), currentState);
+		}
+    }
+    
+    private void saveDebugInfoToSession(final Session session,AZStateNode<String> currentStateNode, Intent incomingIntent){
+    	
+		String previousIntent = (String) session.getAttribute(AZConst.SESSION_KEY_PREVIOUS_INTENT.getValue());
+		String currentState = (String) session.getAttribute(AZConst.SESSION_KEY_CURRENT_STATE.getValue());
+		String debugString = "currentStateAtStartOfTick="+currentState+",previousIntent="+previousIntent+",incomingIntent="+incomingIntent.getName();
+		//See if valid transition matches current state
+		if(currentStateNode.transferStructure.hasValidTransferForIntent(incomingIntent)){
+		    AZStateNode<String> nextState = currentStateNode.transferStructure.getStateForIntent(incomingIntent);
+		    debugString = "newCurrentStateAfterTransition="+nextState.debugName+","+debugString;
+		}
+		String debugHistory = (String) session.getAttribute(AZConst.SESSION_KEY_DEBUG_PATH.getValue());
+		debugHistory = (debugHistory == null) ? "" : debugHistory;		
+		session.setAttribute(AZConst.SESSION_KEY_DEBUG_PATH.getValue(),debugHistory+"\r\n"+debugString);
+		//update the previous intent
+		session.setAttribute(AZConst.SESSION_KEY_PREVIOUS_INTENT.getValue(),incomingIntent.getName());
+		
+		String statePathKey = (String) session.getAttribute(AZConst.SESSION_KEY_STATE_PATH.getValue());
+		statePathKey = (statePathKey == null) ? "" : statePathKey;
+		session.setAttribute(AZConst.SESSION_KEY_STATE_PATH.getValue(),statePathKey+","+currentStateNode.debugName);//path taken in entire session!	
+    }
+    
 }
